2e537782c0b86fcd8253d2e831c836a2b3583f00
0vos <liqianqian1872@gmail.com>
2025-06-23 21:32:16 +0800

    Update 分治生成地图.py

--- 分治生成地图.py
+++ 分治生成地图.py
@@ -1,1 +1,14 @@
-import
+
+def init_maze(height, width):
+    maze = [[' ' for _ in range(width)] for _ in range(height)]
+    return maze
+
+def print_maze(maze):
+    for row in maze:
+        print(''.join(row))
+
+def add_wall(maze, index, direction):
+    if direction == ""
+
+the_maze = init_maze(15, 15)
+print_maze(maze)
