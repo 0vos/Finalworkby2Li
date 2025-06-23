50a8195bc99963e4e1aa29f12bee53617dbe9037
0vos <liqianqian1872@gmail.com>
2025-06-23 21:57:51 +0800

    Update 分治生成地图.py

--- 分治生成地图.py
+++ 分治生成地图.py
@@ -1,4 +1,4 @@
-
+import random
 def init_maze(height, width):
     maze = [[' ' for _ in range(width)] for _ in range(height)]
     return maze
@@ -8,7 +8,30 @@ def print_maze(maze):
         print(''.join(row))
 
 def add_wall(maze, index, direction):
-    if direction == ""
+    if direction == "row":
+        maze[index][:] = '#'
+    if direction == "col":
+        maze[:][index] = '#'
 
+def open_door(maze, row, col):
+    if maze[row][col] == '#':
+        maze[row][col] = ' '
+    else:
+        raise ValueError("已经是通路不需要挖墙")
+
+def get_door_position(maze, row=':', col=':'):
+    if row == ":" and col != ":":
+        width = len(maze[0])
+        row = random.randint(1, width - 2)  # 不包括左右的边界
+        return row, col
+    if col == ":" and row != ":":
+        height = len(maze[0])
+        col = random.randint(1, height - 2)  # 不包括左右的边界
+        return row, col
+
+def dig_maze(maze):
+    height = len(maze)
+    width = len(maze[0])
+    
 the_maze = init_maze(15, 15)
-print_maze(maze)
+print_maze(the_maze)
