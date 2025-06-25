838f44b3264dc972996f9d269da3f718dc3d4f68
0vos <liqianqian1872@gmail.com>
2025-06-25 20:56:46 +0800

    Update 2动态规划路径规划.py

--- 2动态规划路径规划.py
+++ 2动态规划路径规划.py
@@ -1,4 +1,3 @@
-from collections import deque
 the_maze = [
     list("#S#############"),
     list("#OOBOOOOOOO#OO#"),
@@ -77,56 +76,3 @@ def max_coins(maze, start, end):
 start_pos, end_pos = get_start_end(the_maze)
 max_c = max_coins(the_maze, start_pos, end_pos)
 print(max_c)
-
-# def init_dp_table(maze):
-#     dp_table = []
-#     start_position = (0, 0)
-#     exit_position = (0, 0)
-#     for row_index, row in enumerate(maze):
-#         dp_row = []
-#         for col_index, char in enumerate(row):
-#             if char == "#":
-#                 dp_row.append('#')
-#             elif char == "S":
-#                 start_position = (row_index, col_index)
-#                 dp_row.append(0)
-#             elif char == "E":
-#                 exit_position = (row_index, col_index)
-#                 dp_row.append(0)
-#             else:
-#                 dp_row.append(0)
-#         dp_table.append(dp_row)
-#     return dp_table, start_position, exit_position
-#
-# def print2d(maze):
-#     a_maze = []
-#     for row in maze:
-#         tmp_row = []
-#         for elem in row:
-#             if isinstance(elem, int):
-#                 elem = str(elem)
-#             tmp_row.append(elem)
-#         a_maze.append(tmp_row)
-#     for row in a_maze:
-#         print(''.join(row))
-#
-# def count_coins(maze, old_coins, row_index, col_index):
-#     if maze[row_index][col_index] == 'G':
-#         return old_coins + 5
-#     elif maze[row_index][col_index] == 'T':
-#         return old_coins - 3
-#     else:
-#         return old_coins
-#
-# def dp_way(maze, dp_table, start_position, exit_position):
-#     height, width = len(maze), len(maze[0])
-#     coins = 0
-#     i = start_position[0]
-#     j = start_position[1]
-#     while (i, j) != exit_position:
-#         if i - 1 >=0 and dp_table[i - 1][j] == 0:
-#             coins = count_coins(maze, coins, i)
-#
-#
-# the_dp_table, start_pos, exit_pos = init_dp_table(the_maze)
-# print2d(the_dp_table)
